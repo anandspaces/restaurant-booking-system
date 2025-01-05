309db1d9706c55273df31cfae819e7bf263fefd5
anand <anandvaibhaw000@gmail.com>
2025-01-05 22:31:11 +0530

    diabled eslint checks

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // This disables ESLint checks during production builds
+  },
 };
 
 export default nextConfig;
